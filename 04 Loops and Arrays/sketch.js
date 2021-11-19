11aa845904d6c7558d6df3eab7d235e12c69faa6
hudsonjdw <hudsonjdwalters@gmail.com>
2021-11-19 11:57:07 -0600

    Update sketch.js

--- 04 Loops and Arrays/sketch.js
+++ 04 Loops and Arrays/sketch.js
@@ -1,15 +1,45 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Working with Arrays and Loops
+// Mr. Scott
+// Nov 19th, 2021
+// A quick refresher and application of for loops and arrays
 
+//global variables:
+let x = [];      
+let y = [];
+const NUM_POINTS = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  initArrays();
 }
+
+function initArrays(){
+  //we'll use this function to set up all our initial x,y values
+
+}
+
 
 function draw() {
   background(220);
 }
+
+
+
+
+
+
+// function doLoops(){
+  //first:  WHILE Loop
+  
+  // let counter = 0;
+  // while (counter < 6){
+  //   print(names[counter]);
+  //   counter++;
+  // } 
+
+  //second: FOR Loop  (basic)   (later, enhanced)
+  // for(let i = 0 ; i < names.length ; i++){
+  //   print(names[i]);
+  // }
+
+// }
