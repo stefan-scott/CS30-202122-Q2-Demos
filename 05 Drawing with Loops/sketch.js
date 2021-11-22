1f0254aa33e35663369c58e425fba2b1f27ab306
hudsonjdw <hudsonjdwalters@gmail.com>
2021-11-22 12:08:16 -0600

    Update sketch.js

--- 05 Drawing with Loops/sketch.js
+++ 05 Drawing with Loops/sketch.js
@@ -1,9 +1,7 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Drawing with Loops
+// Mr. Scott
+// November 22, 2021
+// Using Loops to draw more complex images
 
 
 function setup() {
@@ -11,5 +9,19 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(255);
+  
+  loopOne();
+  //at the bottom of the the loop, a single new image is drawn
+}
+
+function loopOne() {
+  //use a loop to draw some shapes
+  let spacing = map(mouseX,0,width,10,100);   // can be 0  
+  for (let x = 0; x < width; x += spacing) {
+     //map a fill value
+     let fillValue = map(x,0,width,0,255);
+     fill(fillValue); 
+     circle(x, height/2, spacing);
+  }
 }
