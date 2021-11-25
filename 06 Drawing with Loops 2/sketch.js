03a6e6ed54be8a44784aee93bce51bfe59b9057f
srushtivyas18 <srvkrv1812@gmail.com>
2021-11-25 09:20:52 -0600

    Update sketch.js

--- 06 Drawing with Loops 2/sketch.js
+++ 06 Drawing with Loops 2/sketch.js
@@ -1,15 +1,27 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// Loop Drawing Two
+let spacing = 25;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //noLoop();  //draw run only once
 }
 
 function draw() {
   background(220);
+  nestedLoops();
+}
+
+function nestedLoops(){
+  //to see what happens with a loop inside a loop
+  for (let x = 0; x < width ; x += spacing){
+    for (let y = 0; y < height; y+= spacing){
+      // print(x + ", " + y);
+      circle(x,y,2);
+      let r = map(mouseX, 0, width, 0, 255);
+      let g = map(mouseY, 0, height, 255, 0);
+      let b = map(mouseX, 0, width, 255, 0);
+      stroke(r,g,b);
+      line(x,y,mouseX, mouseY);
+    }
+  }
 }
