653a3a696a96c6e7e70295007d97027cb88288a4
srushtivyas18 <srvkrv1812@gmail.com>
2021-12-01 08:50:08 -0600

    Update sketch.js

--- 09 Terrain Starter Code/sketch.js
+++ 09 Terrain Starter Code/sketch.js
@@ -1,15 +1,24 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Terrain Starter Code
+// 
+// 
 
+let rectWidth=50;   //width of each rectangle slice
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CORNERS);
+  background(220);
+  regularRandomTerrain();
 }
 
 function draw() {
-  background(220);
+  
+}
+
+function regularRandomTerrain(){
+  //create some random terrain using random()
+  for(let x = 0; x < width; x += rectWidth){
+    let sectionHeight = random(0,height);
+    rect(x,height, x+rectWidth, height - sectionHeight);
+  }
 }
