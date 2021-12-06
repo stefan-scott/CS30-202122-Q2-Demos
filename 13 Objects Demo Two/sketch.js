84f6eb5a9458cf8265eee5d1141b832638996c87
stefan-scott <scottst@spsd.sk.ca>
2021-12-06 10:35:40 -0600

    in progress

--- 13 Objects Demo Two/sketch.js
+++ 13 Objects Demo Two/sketch.js
@@ -1,10 +1,7 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// Objects Demo Two
+// Mr. Scott
+// Dec 6th, 2021
+// Objects and Loops working together. Maybe some gravity.
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +9,31 @@ function setup() {
 
 function draw() {
   background(220);
+}
+
+
+class Dot{
+  //class constructor
+  constructor(x, y, diameter){
+    this.x = x;
+    this.y = y;
+    this.diameter = y;  
+    this.c = color(255,255,255);
+  }
+
+  //class methods
+  display(){
+    fill(this.c);
+    circle(this.x, this.y, this.diameter);
+  }
+
+  setColor(){
+    //
+  }
+
+  process(){
+    //helper function to call the appropriate class methods
+    this.setColor();
+    this.display();
+  }
 }
