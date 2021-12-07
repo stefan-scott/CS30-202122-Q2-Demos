ff15f0ee7a3b66640b7295ce2f444c15b8c81b42
stefan-scott <scottst@spsd.sk.ca>
2021-12-07 12:07:14 -0600

    Update sketch.js

--- 14 Vectors Demo/sketch.js
+++ 14 Vectors Demo/sketch.js
@@ -15,12 +15,23 @@ function mousePressed(){
 
 function draw() {
   background(220);
+  movers.push(new Mover(mouseX, mouseY));
+  
   //enhanced for loop
   for(let i = 0; i < movers.length; i++){
     let m = movers[i];
     m.move();
     m.display();
+
+    //what about removal??
+    //if(m.isAlive === false)
+    if(!m.isAlive()){
+      movers.splice(i, 1);
+      i--;
+    }
   }
+
+
 }
 
 class Mover{
@@ -28,9 +39,10 @@ class Mover{
   constructor(x, y){
     this.size = 20;
     this.position = createVector(x,y);
-    this.velocity = createVector(random(-3,3),random(-3,-1));
-    this.acceleration = createVector(0,0);
+    this.velocity = createVector(random(-3,3),random(-10,-5));
+    this.acceleration = createVector(0,0.2);
     this.c = color(random(200), random(200), random(100));
+    this.alive = true;
   }
 
   move(){
@@ -38,6 +50,9 @@ class Mover{
     this.velocity.add(this.acceleration);
     this.position.add(this.velocity);
     this.velocity.limit(5);  //max vel of 5 px/frame
+    if(this.position.y > height){
+      this.alive = false;
+    }
   }
 
   display(){
@@ -46,6 +61,10 @@ class Mover{
     translate(this.position.x, this.position.y);
     circle(0,0,this.size);
     pop();
+  }
+
+  isAlive(){ //can be used directly in an if statement
+    return this.alive;
   }
 
 }
