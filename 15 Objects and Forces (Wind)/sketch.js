6e0e6f882d2b11544e8e32ed9c50230d1ac5a5ec
stefan-scott <scottst@spsd.sk.ca>
2021-12-16 11:41:21 -0600

    Update sketch.js

--- 15 Objects and Forces (Wind)/sketch.js
+++ 15 Objects and Forces (Wind)/sketch.js
@@ -4,16 +4,31 @@
 // More practice with Objects, Vectors, etc...
 
 let myWind;
+let particles = [];
+let grav;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   let windForce = createVector(0,-0.1);
   myWind = new WindZone(width * 0.3, width * 0.5, windForce);
+  grav = createVector(0,0.06);
 }
 
 function draw() {
   background(220);
   myWind.runWind();
+  for(let p of particles){
+    p.applyForce(grav);
+    if(p.isInsideWind(myWind)){
+      p.applyForce(myWind.force);
+    }
+    p.move();
+    p.display();
+  }
+}
+
+function mousePressed(){
+  particles.push(new Particle(mouseX, mouseY));
 }
 
 
@@ -67,6 +82,39 @@ class WindLine{
   display(){
     line(this.pos.x, this.pos.y, this.pos.x, this.pos.y - this.len);
   }
+}
 
+class Particle{
+  //constructor
+  constructor(x, y){
+    this.pos = createVector(x,y);
+    this.vel = createVector(random(3,5),0);
+    this.accel = createVector(0,0);
+  }
 
+  //class methods
+  applyForce(f){
+    //f → vector of a force to be applied on the current frame
+    this.accel.add(f);
+  }
+
+  move(){
+    this.vel.add(this.accel);
+    this.pos.add(this.vel);
+    this.accel.mult(0);   //accel.x *= 0  accel.y *= 0
+  }
+
+  display(){
+    circle(this.pos.x, this.pos.y, 30);
+  }
+
+  isInsideWind(w){
+    //w→ windZone object
+    if(this.pos.x >= w.startX && this.pos.x <= w.endX){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 }
