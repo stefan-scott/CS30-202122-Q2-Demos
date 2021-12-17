1ea0ff160e1a53ad2420d7f7dd097fc84eb277f7
stefan-scott <scottst@spsd.sk.ca>
2021-12-17 12:12:05 -0600

    Update sketch.js

--- 16 Movable Markers and Voronoi/sketch.js
+++ 16 Movable Markers and Voronoi/sketch.js
@@ -4,6 +4,9 @@
 //
 // For a buisiness placement problem...?
 
+let markers = [];
+let currentlyDragging = false; //global variable flag for 
+//                              if we are currently moving something
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,25 +14,53 @@ function setup() {
 
 function draw() {
   background(220);
+  for(let m of markers){
+    m.move();
+    m.display();
+  }
 }
 
-
+function keyPressed(){
+  if(key===" "){
+    markers.push(new MovableMarker(mouseX, mouseY));
+  }
+}
 
 class MovableMarker{
   //something like a pin that can placed/moved on a map
   constructor(x, y){
     this.x = x;
     this.y = y;
+    this.offX = 0;
+    this.offY = 0;  //used for when dragging not from the center
     this.baseColor = color(255,0,0);
     this.hoverColor = color(200,0,0);
     this.beingDragged = false; 
-    this.radius = 15;
+    this.radius = 7;
     this.diameter = this.radius * 2;
   }
 
 
   //class Methods
   move(){
+    if(this.mouseIsOver() && currentlyDragging === false){
+      if(mouseIsPressed && mouseButton === LEFT){
+        this.beingDragged = true;
+        currentlyDragging = true; //prevent other objects from being dragged
+        this.offX = mouseX - this.x;
+        this.offY = mouseY - this.y;
+      }
+    }
+    //check if drag is over
+    if (!mouseIsPressed){
+      this.beingDragged = false;
+      currentlyDragging = false;
+    }
+
+    if(this.beingDragged){
+      this.x = mouseX - this.offX ;
+      this.y = mouseY - this.offY;
+    }
 
   }
 
