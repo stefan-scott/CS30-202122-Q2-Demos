aaf8fdc5819f906a909e190d5cbfa7f2d5e34f23
stefan-scott <scottst@spsd.sk.ca>
2021-12-17 11:47:49 -0600

    Update sketch.js

--- 16 Movable Markers and Voronoi/sketch.js
+++ 16 Movable Markers and Voronoi/sketch.js
@@ -1,9 +1,8 @@
-// Project Title
-// Your Name
-// Date
+// Movable Marks + Voronoi Diagram
+// Mr. Scott
+// Dec 17, 2021
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// For a buisiness placement problem...?
 
 
 function setup() {
@@ -12,4 +11,48 @@ function setup() {
 
 function draw() {
   background(220);
+}
+
+
+
+class MovableMarker{
+  //something like a pin that can placed/moved on a map
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.baseColor = color(255,0,0);
+    this.hoverColor = color(200,0,0);
+    this.beingDragged = false; 
+    this.radius = 15;
+    this.diameter = this.radius * 2;
+  }
+
+
+  //class Methods
+  move(){
+
+  }
+
+  display(){
+    if(this.mouseIsOver()){
+      fill(this.hoverColor);
+    }
+    else{  //no hover
+      fill(this.baseColor);
+    }
+    circle(this.x, this.y, this.diameter);
+  }
+
+  mouseIsOver(){
+    //return true if the mouse is hovering over this shape
+    let d = dist(mouseX, mouseY, this.x, this.y);
+    if(d <= this.radius){
+      return true; //mouse is hovering over this object
+    }
+    else{
+      return false;
+    }
+  }
+  
+
 }
