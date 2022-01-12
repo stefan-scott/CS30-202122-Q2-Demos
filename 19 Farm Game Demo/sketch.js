72ef724c1b4e919b2f9a8989fbfcb0256f30185e
stefan-scott <scottst@spsd.sk.ca>
2022-01-12 12:17:04 -0600

    Update sketch.js

--- 19 Farm Game Demo/sketch.js
+++ 19 Farm Game Demo/sketch.js
@@ -13,7 +13,7 @@ let playerY = 4;
 let level = [
 [0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0],
+[0, 0, 1, 0, 0],
 [0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0] ];
 
@@ -49,6 +49,42 @@ function swap(x1, y1, x2, y2){
   let temp = level[y1][x1]; //back it up
   level[y1][x1] = level[y2][x2];
   level[y2][x2] = temp;
+}
+
+function keyPressed(){
+  if (keyCode === UP_ARROW){
+    if(playerY>0){  //ensures there is at least one tile above
+      //check what item is above us:
+      if(level[playerY-1][playerX]===0){   //empty square above
+        swap(playerX, playerY, playerX, playerY-1);
+        playerY--;
+      }
+      else if(level[playerY-1][playerX]===1){ //chicken
+        if(playerY > 1 && level[playerY-2][playerX]===0){
+          //indicate room to move and not go off array or bump into something
+          swap(playerX, playerY-1, playerX, playerY-2);
+          swap(playerX, playerY, playerX, playerY-1);
+          playerY--;
+        }  
+      }
+      
+
+    }
+  }
+  if (keyCode === DOWN_ARROW){
+    swap(playerX, playerY, playerX, playerY+1);
+    playerY++;
+  }
+  if (keyCode === LEFT_ARROW){
+    swap(playerX, playerY, playerX-1, playerY);
+    playerX--;
+  }
+  if (keyCode === RIGHT_ARROW){
+    swap(playerX, playerY, playerX+1, playerY);
+    playerX++;
+  }
+
+
 }
 
 function draw() {
