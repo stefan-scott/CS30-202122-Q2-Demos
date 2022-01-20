65a8631ab9ac191cf56096bde08e032ec1ffce00
stefan-scott <scottst@spsd.sk.ca>
2022-01-20 01:17:24 -0600

    Update sketch.js

--- 20 Video Demo and Effect/sketch.js
+++ 20 Video Demo and Effect/sketch.js
@@ -22,31 +22,45 @@ function multiVideo() {
   }
 }
 
-function average(pos){
+function average(pos) {
   //return the average value of r/g/b starting at pos
   let r = pixels[pos];
-  let g = pixels[pos+1];
-  let b = pixels[pos+2];
-  return (r+g+b)/3;
+  let g = pixels[pos + 1];
+  let b = pixels[pos + 2];
+  return (r + g + b) / 3;
 }
 
-function setPixelColor(pos, r,g,b){
+function setPixelColor(pos, r, g, b) {
   //Helper Function to set RGB for particular pixel, index of R given by pos
   pixels[pos] = r;
-  pixels[pos+1] = g;
-  pixels[pos+2] = b;
+  pixels[pos + 1] = g;
+  pixels[pos + 2] = b;
+}
+
+function magnify() {
+  let cursorArea = video.get(mouseX - 20, mouseY - 20, 40, 40)
+  rectMode(CENTER)
+  rect(mouseX, mouseY, cursorArea.width * 3 + 4, cursorArea.height * 3 + 4);
+  image(cursorArea, mouseX, mouseY, cursorArea.width * 3, cursorArea.height * 3);
 }
 
 function draw() {
   background(220);
   //multiVideo();
+  imageMode(CORNER);
+  rectMode(CORNER);
+  image(video, 0, 0);
 
-  video.loadPixels();  //tells p5 we want access to the pixel array
-  for(let i = 0; i < pixels.length; i += 4){
+  imageMode(CENTER);
+  magnify();
+
+  loadPixels();  //tells p5 we want access to the pixel array
+  for (let i = 0; i < pixels.length; i += 4) {
     let avg = average(i);
     setPixelColor(i, avg, avg, avg);
   }
 
-  video.updatePixels();
-  image(video, 0,0);
+  updatePixels();
+
+
 }
